e2aa89a2ec3696fa33cf177d6650abe89480ac48
Josecholi <41088444+Josecholi@users.noreply.github.com>
2019-01-09 19:42:00 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL ^ E_NOTICE);
 
 //database
-$db_host = 'localhost';
-$db_name = 'agame';
-$db_user = 'root';
-$db_password = '';
+$db_host = 'ec2-174-129-18-247.compute-1.amazonaws.com';
+$db_name = 'd14dugak9a8n8i';
+$db_user = 'agkejvbnsugkri';
+$db_password = '4b6a18bd42df2fc1c5cbbe1155b7e2777bc1c8d5d4344bd6034eccece813ddd4';
 
 ?>
